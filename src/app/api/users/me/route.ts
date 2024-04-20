9e13cc7c3987d408ae59705c8928d426902edb28
sobhan9012 <sobhansahoo2000@gmail.com>
2024-04-21 00:37:39 +0530

    adding route for user info or view somthing and for this route add getdatafromToken from

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,29 @@
+import { connect } from '@/dbConfig/dbConfig';
+import User from '@/models/userModel'
+import { NextRequest,NextResponse } from 'next/server'
+import { getDataFromToken } from '@/helpers/getDataFromToken';
+ 
+connect() 
+
+
+export async function POST(request : NextRequest) { 
+
+    try {
+
+        //extract data from token
+        const userId = await getDataFromToken(request)
+
+        const user = await User.findOne({_id : userId}).select("-password")
+
+        //check fi there is no user
+
+        return NextResponse.json({
+            message : "user found",
+            data: user
+        })
+
+    } catch(error : any) {
+
+    }
+
+}
